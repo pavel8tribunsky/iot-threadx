7f808993597fdd6274febcfd6863fc75c990d982
Pavel Tribunsky <pavel.tribunsky@gmail.com>
2025-09-05 02:15:04 +0300

    Add PE9 as TIM1_CHN1 out for LED connection

--- firmware/AZURE_RTOS/App/app_azure_rtos.c
+++ firmware/AZURE_RTOS/App/app_azure_rtos.c
@@ -51,12 +51,12 @@ static TX_BYTE_POOL tx_app_byte_pool;
 
 /* USER CODE BEGIN FX_Pool_Buffer */
 /* USER CODE END FX_Pool_Buffer */
-static UCHAR fx_byte_pool_buffer[FX_APP_MEM_POOL_SIZE];
+static UCHAR  fx_byte_pool_buffer[FX_APP_MEM_POOL_SIZE];
 static TX_BYTE_POOL fx_app_byte_pool;
 
 /* USER CODE BEGIN NX_Pool_Buffer */
 /* USER CODE END NX_Pool_Buffer */
-static UCHAR nx_byte_pool_buffer[NX_APP_MEM_POOL_SIZE];
+static UCHAR  nx_byte_pool_buffer[NX_APP_MEM_POOL_SIZE];
 static TX_BYTE_POOL nx_app_byte_pool;
 
 /* USER CODE BEGIN PV */
@@ -70,11 +70,12 @@ void my_thread_entry(ULONG thread_input);
 /* USER CODE END PFP */
 
 /**
- * @brief  Define the initial system.
- * @param  first_unused_memory : Pointer to the first unused memory
- * @retval None
- */
-VOID tx_application_define(VOID *first_unused_memory) {
+  * @brief  Define the initial system.
+  * @param  first_unused_memory : Pointer to the first unused memory
+  * @retval None
+  */
+VOID tx_application_define(VOID *first_unused_memory)
+{
   /* USER CODE BEGIN  tx_application_define */
   tx_thread_create(&my_thread, "My Thread", my_thread_entry, 0x1234,
                    first_unused_memory, 1024, 3, 3, TX_NO_TIME_SLICE,
@@ -83,20 +84,22 @@ VOID tx_application_define(VOID *first_unused_memory) {
 
   VOID *memory_ptr;
 
-  if (tx_byte_pool_create(&tx_app_byte_pool, "Tx App memory pool",
-                          tx_byte_pool_buffer,
-                          TX_APP_MEM_POOL_SIZE) != TX_SUCCESS) {
+  if (tx_byte_pool_create(&tx_app_byte_pool, "Tx App memory pool", tx_byte_pool_buffer, TX_APP_MEM_POOL_SIZE) != TX_SUCCESS)
+  {
     /* USER CODE BEGIN TX_Byte_Pool_Error */
 
     /* USER CODE END TX_Byte_Pool_Error */
-  } else {
+  }
+  else
+  {
     /* USER CODE BEGIN TX_Byte_Pool_Success */
 
     /* USER CODE END TX_Byte_Pool_Success */
 
     memory_ptr = (VOID *)&tx_app_byte_pool;
 
-    if (App_ThreadX_Init(memory_ptr) != TX_SUCCESS) {
+    if (App_ThreadX_Init(memory_ptr) != TX_SUCCESS)
+    {
       /* USER CODE BEGIN  App_ThreadX_Init_Error */
 
       /* USER CODE END  App_ThreadX_Init_Error */
@@ -105,22 +108,25 @@ VOID tx_application_define(VOID *first_unused_memory) {
     /* USER CODE BEGIN  App_ThreadX_Init_Success */
 
     /* USER CODE END  App_ThreadX_Init_Success */
+
   }
 
-  if (tx_byte_pool_create(&fx_app_byte_pool, "Fx App memory pool",
-                          fx_byte_pool_buffer,
-                          FX_APP_MEM_POOL_SIZE) != TX_SUCCESS) {
+  if (tx_byte_pool_create(&fx_app_byte_pool, "Fx App memory pool", fx_byte_pool_buffer, FX_APP_MEM_POOL_SIZE) != TX_SUCCESS)
+  {
     /* USER CODE BEGIN FX_Byte_Pool_Error */
 
     /* USER CODE END FX_Byte_Pool_Error */
-  } else {
+  }
+  else
+  {
     /* USER CODE BEGIN FX_Byte_Pool_Success */
 
     /* USER CODE END FX_Byte_Pool_Success */
 
     memory_ptr = (VOID *)&fx_app_byte_pool;
 
-    if (MX_FileX_Init(memory_ptr) != FX_SUCCESS) {
+    if (MX_FileX_Init(memory_ptr) != FX_SUCCESS)
+    {
       /* USER CODE BEGIN MX_FileX_Init_Error */
 
       /* USER CODE END MX_FileX_Init_Error */
@@ -131,20 +137,22 @@ VOID tx_application_define(VOID *first_unused_memory) {
     /* USER CODE END MX_FileX_Init_Success */
   }
 
-  if (tx_byte_pool_create(&nx_app_byte_pool, "Nx App memory pool",
-                          nx_byte_pool_buffer,
-                          NX_APP_MEM_POOL_SIZE) != TX_SUCCESS) {
+  if (tx_byte_pool_create(&nx_app_byte_pool, "Nx App memory pool", nx_byte_pool_buffer, NX_APP_MEM_POOL_SIZE) != TX_SUCCESS)
+  {
     /* USER CODE BEGIN NX_Byte_Pool_Error */
 
     /* USER CODE END NX_Byte_Pool_Error */
-  } else {
+  }
+  else
+  {
     /* USER CODE BEGIN TX_Byte_Pool_Success */
 
     /* USER CODE END TX_Byte_Pool_Success */
 
     memory_ptr = (VOID *)&nx_app_byte_pool;
 
-    if (MX_NetXDuo_Init(memory_ptr) != NX_SUCCESS) {
+    if (MX_NetXDuo_Init(memory_ptr) != NX_SUCCESS)
+    {
       /* USER CODE BEGIN MX_NetXDuo_Init_Error */
 
       /* USER CODE END MX_NetXDuo_Init_Error */
@@ -153,7 +161,9 @@ VOID tx_application_define(VOID *first_unused_memory) {
     /* USER CODE BEGIN MX_NetXDuo_Init_Success */
 
     /* USER CODE END MX_NetXDuo_Init_Success */
+
   }
+
 }
 
 /* USER CODE BEGIN  0 */
